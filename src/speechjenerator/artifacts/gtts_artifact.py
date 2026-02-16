eb3645922a617cdc8f6967c9bb07ea9a920ed64b
Warren Davies <warren_davies81@hotmail.com>
2026-02-16 20:42:27 +0000

    updated GTTSArtifact with play method

--- src/speechjenerator/artifacts/gtts_artifact.py
+++ src/speechjenerator/artifacts/gtts_artifact.py
@@ -1,3 +1,7 @@
+import os
+import platform
+import subprocess
+
 import sounddevice as sd
 import soundfile as sf
 
@@ -5,20 +9,29 @@ from basejenerator.artifacts.base_artifact import BaseArtifact
 
 class GTTSArtifact(BaseArtifact):
     """
-    Holds a gTTS object. This is more like a promise to create an audio file.
+    Holds a gTTS "model" object. This is more like a promise to create an audio file.
     Due to how gTTS works, the Google Translate API is not called until `.save()`
     is called.
 
     Attributes:
-        data (str): The gTTS object.
+        data (str): The gTTS "model" object.
         item_extras (dict): any additional metadata related to this artifact
     """
 
     def __init__(self, data, item_extras):
         self.data = data 
         self.item_extras = item_extras
-        self.extension = ".wav"
-
+        self.extension = ".mp3"
+        self.system = platform.system()
+        self.linux_mp3_players = [
+            ["mpg123", "--quiet"],
+            ["mpv", "--no-terminal"],
+        ]
+        self.player_funcs = {
+            "Windows": self._play_on_windows,
+            "Darwin": self._play_on_mac,
+            "Linux": self._play_on_linux
+        }
 
     def save(self, path):
         """
@@ -27,13 +40,33 @@ class GTTSArtifact(BaseArtifact):
         self.data.save(path)
 
 
+    def _play_on_windows(self, path):
+        os.startfile(path)
+
+    def _play_on_mac(self, path):
+        subprocess.run(['afplay', path])
+
+    def _play_on_linux(self, path):
+        for player_command in self.linux_mp3_players:
+            player_command.append(path)
+            try:
+                subprocess.run(player_command, check=True)
+                break
+            except FileNotFoundError:
+                continue
+
     def play(self, path=None):
         """
-        Called Google Translate API, gets the audio as WAV, and saves to disk.
+        Calls Google Translate API, gets the audio as mp3, saves to disk,
+        and plays the audio on the system in which speechjenerator is running
+        using .
         """
         if not path:
             path = self.data["config"]["path"]
         self.save(path)
-        data, fs = sf.read(path, dtype='float32')
-        sd.play(data, fs)
-        sd.wait()
+
+        mp3_player = self.player_funcs[self.system]
+        try:
+            mp3_player(path)
+        except Exception as e:
+            print(f"Error playing audio: {e}")
--- src/speechjenerator/examples/run_inference.py
+++ src/speechjenerator/examples/run_inference.py
@@ -4,13 +4,14 @@ output_path = "./outputs/test.wav"
 config = {
     "model": "gtts",
 
-    "text": "Do or do not - there is no try.",
-    "lang": "en"
+    "text": "Hazlo o no lo hagas. Intentar no existe.",
+    "lang": "es"
 
 }
 
 speech_generator = registry.get_model_class(config)
 speech_generator.load()
+print(speech_generator.config)
 output = speech_generator.generate()
 speech = output.batch[0]
 speech.play(output_path)
